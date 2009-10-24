004b3a3c10bc27c8df3e2e327327494a4d7cacc0
Kevin.Ridsdale <Kevin.Ridsdale@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-10-24 20:02:04 +0000

    Migrating to proper structure

---
+++ trunk/OHMS/WEB-INF/classes/models/database/Account_Keeper.java
@@ -0,0 +1,40 @@
+package database;
+
+import messages.*;
+import java.util.*;
+
+public class Account_Keeper {
+	// People logged into the system
+	private Vector c_Accounts;
+
+	public void process_Message(Account_Message i_msg){
+		System.err.print("in keeper");
+		i_msg.print_Middle();
+	}
+
+	private void Add()
+	{
+
+	}
+
+	private void Edit()
+	{
+
+	}
+
+	private void Delete()
+	{
+
+	}
+
+	private void Authentication()
+	{
+
+	}
+
+	private void View()
+	{
+
+	}
+
+}
