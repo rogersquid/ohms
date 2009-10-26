147c7f74e3cea7cd97c27a447fcc176a53c012bb
mertcos <mertcos@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-10-26 23:16:19 +0000

    edit account

--- branches/DatabaseTesting/OHMS/src/database/Account.java
+++ branches/DatabaseTesting/OHMS/src/database/Account.java
@@ -5,12 +5,11 @@ import java.util.*;
 import java.sql.*;
 import java.security.*;
 import messages.*;
+import messages.message_Helper.Header;
 import misc.*;
 import database.database_Helper.*;
 
 public class Account {
-
-
 	private int c_Account_ID;
 	private String c_Account_Type;
 	private String c_F_name;
@@ -22,18 +21,17 @@ public class Account {
 	private String c_Address;
 	private java.util.Date c_Date;
 
-	private String dname = "default_hotel";
+	private String dbname = "default_hotel";
 
 	public boolean login(String username, String password) {
 		return accountIsValid(username, password);
 	}
-
 	public boolean accountIsValid(String username, String password) {
 		String md5_password = MD5.hashString(password);
 		boolean result = false;
 		database_Helper dbcon = null;
 		try {
-			dbcon = new database_Helper(dname);
+			dbcon = new database_Helper(dbname);
 			String query = "SELECT * FROM users WHERE email='" + username + "' AND password='" + md5_password + "'";
 			ResultSet rs = dbcon.select(query);
 
@@ -69,7 +67,7 @@ public class Account {
 		boolean result = false;
 		database_Helper dbcon = null;
 		try {
-			dbcon = new database_Helper(dname);
+			dbcon = new database_Helper(dbname);
 			ResultSet returnedSet = dbcon.select("Select from account where email='" + i_email + "'");
 			if (returnedSet.first()) {
 				c_Account_ID = (int)returnedSet.getDouble("accountID");
@@ -101,14 +99,11 @@ public class Account {
 
 	// Creates a unique ID, inserts today's date
 	// Returns true if successful, false if error
-	public boolean Add_Account(String i_F_name,
-			String i_S_name, boolean i_Gender,
-			String i_Phone, String i_Email,
-			String i_Address) 
+	public boolean Add_Account(String i_F_name, String i_S_name, boolean i_Gender,
+			String i_Phone, String i_Email,String i_Address) 
 	{
 		// Verify parameters are valid.
 		// No date error checking implemented 
-
 		boolean result = false;
 		database_Helper dbcon = null;
 		try {
@@ -152,7 +147,7 @@ public class Account {
 		boolean result = false;
 		database_Helper dbcon = null;
 		try {
-			dbcon = new database_Helper(dname);
+			dbcon = new database_Helper(dbname);
 			int returnedRows = dbcon.modify("DELETE FROM account WHERE email = '"
 						+ i_email + "'");
 			if (returnedRows == 1) {
@@ -188,7 +183,7 @@ public class Account {
 		boolean result = false;
 		database_Helper dbcon = null;
 		try {
-			dbcon = new database_Helper(dname);
+			dbcon = new database_Helper(dbname);
 			int returnedRows = dbcon.modify("DELETE FROM account WHERE accountID = '"
 					+ i_Account_ID + "'");
 			if (returnedRows == 1) {
@@ -214,6 +209,52 @@ public class Account {
 				dbcon.close();
 			}
 		}
+		return result;
+	}
+	
+	public boolean Edit_Account(Account_Message i_msg){
+			// Verify parameters are valid.
+			// No date error checking implemented 
+			boolean result = false;
+			int g;
+			if(i_msg.gender)g=1;
+			else g=0;
+			database_Helper dbcon = null;
+			try {
+				dbcon = new database_Helper(dbname);
+				java.util.Date now = new java.util.Date();
+				int returnedRows = dbcon.modify("UPDATE account SET firstName='" + i_msg.firstname
+						+ "', lastName='" +i_msg.lastname + "', gender='" + g
+						+ "', phone='" + i_msg.phone + "', email='" + i_msg.email
+						+ "', address='" + i_msg.address + "' "
+						+ "WHERE accountID='" + i_msg.account_id + "'");
+				if (returnedRows == 1) {
+					i_msg.fill_Header_Response(Header.Response.SUCCESS, "Updated One Account as Requested." +
+							" Account Email: " + i_msg.email);
+					result = true;
+				} else if (returnedRows>1) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Updated More than one Account. " +
+							"Indicated inconsistency. Account Email: " + i_msg.email);	
+					result = false;
+				}
+				else {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Update Failed. Nothing changed.");	
+					result = false;
+				}
+			} catch (SQLException e) {
+				System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
+				e.printStackTrace(System.err);
+				result = false;
+			} catch (ClassNotFoundException e) {
+				System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
+				e.printStackTrace(System.err);
+				result = false;
+			}finally
+			{
+				if (dbcon != null) {
+					dbcon.close();
+				}
+			}
 		return result;
 	}
 
