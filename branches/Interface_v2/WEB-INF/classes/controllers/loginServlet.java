9bdb5756c5a41441f0ae61c50c0ef85b966134cd
mayfer <mayfer@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-10-29 22:42:04 +0000

    initial checkout

---
+++ branches/Interface_v2/WEB-INF/classes/controllers/loginServlet.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import models.messages.*;
+import models.messages.message_Helper.*;
+import models.database.*;
+
+public class loginServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		getServletContext().getRequestDispatcher("/views/login_form.jsp").include(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		int userid = 0;
+		int authlevel = 0;
+		String hotelname = "default_hotel";
+		Account_Message message = new Account_Message(userid, authlevel, hotelname, Header.Action.LOGIN);
+		message.email = email;
+		message.password = password;
+		
+		Hotel hotel = new Hotel(hotelname);
+		hotel.process_Message(message);
+		
+		if(message.header.response_code == Header.Response.SUCCESS) {
+			request.setAttribute("email", email);
+			getServletContext().getRequestDispatcher("/views/login_success.jsp").include(request, response);
+		} else {
+			request.setAttribute("status", "login_failed");
+			getServletContext().getRequestDispatcher("/views/login_form.jsp").include(request, response);
+		}
+	}
+
+}
---
+++ branches/Interface_v2/WEB-INF/classes/controllers/registerServlet.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import models.database.*;
+import models.messages.*;
+import models.messages.message_Helper.*;
+
+public class registerServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		getServletContext().getRequestDispatcher("/views/register_form.jsp").include(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		String password = request.getParameter("password");
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		boolean gender = (request.getParameter("gender") == "1") ? true : false;
+		
+		int userid = 1;
+		int authlevel = 3;
+		String hotelname = "default_hotel";
+		Account_Message message = new Account_Message(userid, authlevel, hotelname, Header.Action.ADD);
+		message.email = email;
+		message.password = password;
+		message.firstname = firstname;
+		message.lastname = lastname;
+		message.phone = phone;
+		message.address = address;
+		message.gender = gender;
+		
+		Hotel hotel = new Hotel(hotelname);
+		hotel.process_Message(message);
+		if(message.header.response_code == Header.Response.SUCCESS) {
+			request.setAttribute("firstname", firstname);
+			request.setAttribute("lastname", lastname);
+			getServletContext().getRequestDispatcher("/views/register_success.jsp").include(request, response);
+		} else {
+			request.setAttribute("status", "register_failed");
+			request.setAttribute("message", message.header.response_string);
+			getServletContext().getRequestDispatcher("/views/register_form.jsp").include(request, response);
+		}
+	}
+
+}
