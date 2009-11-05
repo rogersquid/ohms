cfe0ab9a7b31a9a2bf6faaac4c5e06c7d6896834
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-05 00:27:49 +0000

    added authorization class

---
+++ branches/Database_v2/OHMS/src/models/database/Authorization.java
@@ -0,0 +1,53 @@
+package models.database;
+import java.sql.SQLException;
+
+import models.messages.*;
+import java.sql.*;
+public class Authorization {
+	
+	public String dbname = "default_hotel";
+	
+	public AuthMessage getAuthLevel(AuthMessage i_msg)
+	{
+		
+		AuthMessage reply = null;
+		
+		databaseHelper 	dbcon 	= null;
+		try {
+			dbcon 				= new databaseHelper(dbname);
+			
+			ResultSet results = dbcon.select("SELECT authLevel FROM account WHERE accountID="+ i_msg.account_id);
+			
+			if (results.first()) {
+				i_msg.authLevel = results.getInt(results.findColumn("authLevel"));
+				i_msg.fill_Header_Response(Header.Response.SUCCESS, "Got authorization level for" +
+						" Account Email: " + i_msg.email);
+				reply	= i_msg;
+			} else {
+				i_msg.fill_Header_Response(Header.Response.FAIL, "Authorization check failed for" +
+						" Account ID: " + i_msg.account_id);
+				reply	= i_msg;
+			}
+		} catch (SQLException e) {
+			System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
+			e.printStackTrace(System.err);
+			i_msg.fill_Header_Response(Header.Response.FAIL, "Authorization check failed for" +
+					" Account ID: " + i_msg.account_id);
+			reply	= i_msg;
+		} catch (ClassNotFoundException e) {
+			System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
+			e.printStackTrace(System.err);
+			i_msg.fill_Header_Response(Header.Response.FAIL, "Authorization check failed for" +
+					" Account ID: " + i_msg.account_id);
+			reply	= i_msg;
+		}
+		finally {
+			if (dbcon != null) {
+				dbcon.close();
+			}
+		}
+		return reply.deepCopy();
+		
+	}
+	
+}
