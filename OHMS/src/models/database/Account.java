3910a5d553292b8ac12ea011392efa7efaaa0eb9
michael.soderling <michael.soderling@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-07 21:40:11 +0000

    Fixed Account.java for header updates

--- OHMS/src/models/database/Account.java
+++ OHMS/src/models/database/Account.java
@@ -10,7 +10,7 @@ public class Account {
 	String dbname = "ohms";
 	// Creates a unique ID, inserts today's date
 	// Returns true if successful, false if error
-	
+
 	/* ReMINDER FROM MERT:
 	 * we need a viewALL method that return all the accounts
 	 * so that the interface can display them
@@ -25,96 +25,130 @@ public class Account {
 	}
 	public AccountMessage addAccount(AccountMessage i_msg) {
 		// Verify parameters are valid.
+//		i_msg = validateParams(i_msg);
+
 		// No date error checking implemented 
 		AccountMessage 	reply;
-		String 			pwmd	= MD5.hashString(i_msg.password);
-		int 			genderInt;
-		if(i_msg.gender){
-			genderInt = 1;
-		}
-		else {
-			genderInt = 0;
-		}
-		databaseHelper dbcon 	= null;
-		try {
-			dbcon 				= new databaseHelper(dbname);
-			java.util.Date now 	= new java.util.Date();
-			int returnedRows 	= dbcon.modify("INSERT INTO account (firstName, lastName, accountType, password, gender, phone, email, address, date) VALUES ('" 
-					+ i_msg.firstname
-					+ "', '" + i_msg.lastname + "', '" 
-					+ "customer" + "', '"
-					+ pwmd + "', '" + genderInt
-					+ "', '" + i_msg.phone + "', '" + i_msg.email
-					+ "', '" + i_msg.address + "', '" + now + "')");
-			if (returnedRows == 1) {
-				i_msg.fillHeaderResponse(Header.Response.SUCCESS, "Added one Account as Requested." +
-						" Account Email: " + i_msg.email);
-				reply	= i_msg;
-			}else {
+		if (i_msg.returnHeader().responseCode != Header.Response.FAIL) 
+		{
+			String pwmd = MD5.hashString(i_msg.password);
+			int genderInt;
+			if (i_msg.gender) {
+				genderInt = 1;
+			} else {
+				genderInt = 0;
+			}
+			databaseHelper dbcon = null;
+			try {
+				dbcon = new databaseHelper(dbname);
+				java.util.Date now = new java.util.Date();
+				int returnedRows = dbcon
+				.modify("INSERT INTO account (firstName, lastName, accountType, password, gender, phone, email, address, date) VALUES ('"
+						+ i_msg.firstname
+						+ "', '"
+						+ i_msg.lastname
+						+ "', '"
+						+ "customer"
+						+ "', '"
+						+ pwmd
+						+ "', '"
+						+ genderInt
+						+ "', '"
+						+ i_msg.phone
+						+ "', '"
+						+ i_msg.email
+						+ "', '"
+						+ i_msg.address
+						+ "', '"
+						+ now + "')");
+				if (returnedRows == 1) {
+					i_msg.fillHeaderResponse(Header.Response.SUCCESS,
+							"Added one Account as Requested."
+							+ " Account Email: " + i_msg.email);
+				} else {
+					i_msg.fillHeaderResponse(Header.Response.FAIL,
+							"Update failed." + " Account Email: "
+							+ i_msg.email);
+				}
+			}
+			catch (SQLException e) {
+				System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
+				e.printStackTrace(System.err);
 				i_msg.fillHeaderResponse(Header.Response.FAIL, "Update failed." +
 						" Account Email: " + i_msg.email);
-				reply	= i_msg;
+			} catch (ClassNotFoundException e) {
+				System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
+				e.printStackTrace(System.err);
+				i_msg.fillHeaderResponse(Header.Response.FAIL, "Update failed." +
+						" Account Email: " + i_msg.email);
+				
 			}
-		} catch (SQLException e) {
-			System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
-			e.printStackTrace(System.err);
-			i_msg.fillHeaderResponse(Header.Response.FAIL, "Update failed." +
-					" Account Email: " + i_msg.email);
-			reply	= i_msg;
-		} catch (ClassNotFoundException e) {
-			System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
-			e.printStackTrace(System.err);
-			i_msg.fillHeaderResponse(Header.Response.FAIL, "Update failed." +
-					" Account Email: " + i_msg.email);
-			reply	= i_msg;
-		}
-		finally {
-			if (dbcon != null) {
-				dbcon.close();
+			finally {
+				if (dbcon != null) {
+					dbcon.close();
+				}
 			}
 		}
+		reply	= i_msg;
 		return reply.deepCopy();
 	}
+
 	public AccountMessage editAccount(AccountMessage i_msg){
 		// Verify parameters are valid.
+//		i_msg = validateParams(i_msg);
+
 		// No date error checking implemented 
-		AccountMessage 	reply	= null;
-		int			 	genderInt;
-		if(i_msg.gender){
-			genderInt = 1;
-		}
-		else {
-			genderInt = 0;
-		}
-		databaseHelper dbcon = null;
-		try {
-			dbcon = new databaseHelper(dbname);
-			int returnedRows = dbcon.modify("UPDATE account SET firstName='" + i_msg.firstname
-					+ "', lastName='" +i_msg.lastname + "', gender='" + genderInt
-					+ "', phone='" + i_msg.phone + "', email='" + i_msg.email
-					+ "', address='" + i_msg.address + "' "
-					+ "WHERE accountID='" + i_msg.accountID + "'");
-			if (returnedRows == 1) {
-				i_msg.fillHeaderResponse(Header.Response.SUCCESS, "Updated One Account as Requested." +
-						" Account Email: " + i_msg.email);
-			} else if (returnedRows>1) {
-				i_msg.fillHeaderResponse(Header.Response.FAIL, "Updated More than one Account. " +
-						"Indicated inconsistency. Account Email: " + i_msg.email);	
+		AccountMessage 	reply;
+		if (i_msg.returnHeader().responseCode != Header.Response.FAIL) {
+			reply = null;
+			int genderInt;
+			if (i_msg.gender) {
+				genderInt = 1;
+			} else {
+				genderInt = 0;
 			}
-			else {
-				i_msg.fillHeaderResponse(Header.Response.FAIL, "Update Failed. Nothing changed.");	
+			databaseHelper dbcon = null;
+			try {
+				dbcon = new databaseHelper(dbname);
+				int returnedRows = dbcon
+				.modify("UPDATE account SET firstName='"
+						+ i_msg.firstname + "', lastName='"
+						+ i_msg.lastname + "', gender='" + genderInt
+						+ "', phone='" + i_msg.phone + "', email='"
+						+ i_msg.email + "', address='" + i_msg.address
+						+ "' " + "WHERE accountID='" + i_msg.accountID
+						+ "'");
+				if (returnedRows == 1) {
+					i_msg.fillHeaderResponse(Header.Response.SUCCESS,
+							"Updated One Account as Requested."
+							+ " Account Email: " + i_msg.email);
+				} else if (returnedRows > 1) {
+					i_msg.fillHeaderResponse(
+							Header.Response.FAIL,
+							"Updated More than one Account. "
+							+ "Indicated inconsistency. Account Email: "
+							+ i_msg.email);
+				} else {
+					i_msg.fillHeaderResponse(Header.Response.FAIL,
+							"Update Failed. Nothing changed.");
+				}
+			} catch (SQLException e) {
+				System.err
+				.println("Error in 'Add_Account'.  SQLException was thrown:");
+				e.printStackTrace(System.err);
+			} catch (ClassNotFoundException e) {
+				System.err
+				.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
+				e.printStackTrace(System.err);
+			} finally {
+				if (dbcon != null) {
+					dbcon.close();
+				}
+				else {
+					i_msg.fillHeaderResponse(Header.Response.FAIL, "Update Failed. Nothing changed.");	
+				}
 			}
-		} catch (SQLException e) {
-			System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
-			e.printStackTrace(System.err);
-		} catch (ClassNotFoundException e) {
-			System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
-			e.printStackTrace(System.err);
-		}finally
-		{
-			if (dbcon != null) {
-				dbcon.close();
-			}
+
 		}
 		reply = i_msg.deepCopy();
 		return reply;
@@ -184,7 +218,7 @@ public class Account {
 				String r_mail = returnedSet.getString("email"); 
 				String r_add = returnedSet.getString("address"); 
 				String r_date = returnedSet.getString("date"); 
-				
+
 				reply.fill_All(r_account_id, r_account_type, r_first_name, r_surname, "", r_gender, r_phone, r_add, r_mail);
 				DateFormat formatter = DateFormat.getDateInstance();
 				reply.date = formatter.parse(r_date);
@@ -226,4 +260,41 @@ public class Account {
 		return reply.deepCopy();
 	}
 	// authentication is job of authenticator he will  use view.
+
+//	public AccountMessage validateParams(AccountMessage i_msg){
+//		AccountMessage 	reply;
+//
+//		// Verify accountType
+//		// guest, maid, customer, staff, or admin
+//		if(!(i_msg.accountType.equalsIgnoreCase("guest") ||
+//				i_msg.accountType.equalsIgnoreCase("customer") ||
+//				i_msg.accountType.equalsIgnoreCase("maid") ||
+//				i_msg.accountType.equalsIgnoreCase("staff") ||
+//				i_msg.accountType.equalsIgnoreCase("admin")))
+//		{
+//			i_msg.concatHeaderResponse(Header.Response.FAIL, "Invaild accountType: " + i_msg.accountType +
+//					". Account ID: " + i_msg.accountID );
+//		}
+//
+//		// Verify address
+//		String testAddr = i_msg.address;
+//		if(testAddr.isEmpty())
+//		{
+//			i_msg.fillHeaderResponse(Header.Response.FAIL, "Address String should not be empty. " +
+//					"Account ID: " + i_msg.accountID );
+//		}
+//
+//		// Verify email
+////		i_msg.email;
+////
+////
+////		i_msg.firstname;
+////
+////		i_msg.lastname;
+////
+////		i_msg.phone;
+//
+//		return reply;
+//	}
 }
+
