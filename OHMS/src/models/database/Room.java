e0b5ef5bafcb2c3c7a3b721ff6ddbf0111dd6d55
BenSiu <BenSiu@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-07 20:55:15 +0000

    Updated Room Tests to support additional room inventory items and also invalid inputs
    Updated Room Inventory to support the changes to the database table
    
    Updated Room to support changes to the database table and also check for invalid inputs

--- OHMS/src/models/database/Room.java
+++ OHMS/src/models/database/Room.java
@@ -11,41 +11,45 @@ public class Room {
 		RoomMessage reply=null;
 		databaseHelper 	dbcon = null;
 		try {
-			dbcon = new databaseHelper(dbname);
-			
-			ResultSet rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-			if (rs.next()) {
-				i_msg.fill_Header_Response(Header.Response.FAIL, "Room number already in database.");
-				
-				reply	= i_msg;
-			} else if (i_msg.room_type == ""){
+			if (validateParam(i_msg)){
 				i_msg.fill_Header_Response(Header.Response.FAIL, "Invalid input(s).");
 				
 				reply	= i_msg;
 			} else {
-				int insertStatus = dbcon.modify("INSERT INTO test_rooms (roomID, roomNumber, roomFloor, roomType, price, onsuite, " +
-						"tv, disability, elevator, ebirdcall, emornpaper, availability, numBed) VALUES (" + 
-						i_msg.room_id + ", " + 
-						i_msg.room_number + ", " + 
-						i_msg.room_floor + ", '" + 
-						i_msg.room_type + "', " + 
-						i_msg.price + ", " + 
-						i_msg.room_specs.search_Specs("onsuite") + ", " + 
-						i_msg.room_specs.search_Specs("tv") + ", " + 
-						i_msg.room_specs.search_Specs("disability") + ", " + 
-						i_msg.room_specs.search_Specs("elevator") + ", " + 
-						i_msg.room_specs.search_Specs("ebirdcall") + ", " + 
-						i_msg.room_specs.search_Specs("emornpaper") + ", " + 
-						((i_msg.available)?1:0) + ", " + 
-						i_msg.room_specs.search_Specs("numBed") + ")");
-				if (insertStatus == 1) {
-					i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room created in database.");
+				dbcon = new databaseHelper(dbname);
+				Header tempHead = i_msg.return_Header();
+				ResultSet rs = dbcon.select("SELECT * FROM " + tempHead.name_hotel + "_rooms WHERE roomNumber = " + i_msg.room_number);
+				if (rs.next()) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Room number already in database.");
 					
-					reply = i_msg;
+					reply	= i_msg;
 				} else {
-					i_msg.fill_Header_Response(Header.Response.FAIL, "Insert operation failed.");
-					
-					reply = i_msg;
+					int insertStatus = dbcon.modify("INSERT INTO test_rooms (roomID, roomNumber, roomFloor, roomType, price, onsuite, " +
+							"tv, disability, elevator, availability, numBed, phone, internet, kitchen, clean) VALUES (" + 
+							i_msg.room_id + ", " + 
+							i_msg.room_number + ", " + 
+							i_msg.room_floor + ", '" + 
+							i_msg.room_type + "', " + 
+							i_msg.price + ", " + 
+							i_msg.room_specs.search_Specs("onsuite") + ", " + 
+							i_msg.room_specs.search_Specs("tv") + ", " + 
+							i_msg.room_specs.search_Specs("disability") + ", " + 
+							i_msg.room_specs.search_Specs("elevator") + ", " + 
+							((i_msg.available)?1:0) + ", " + 
+							i_msg.room_specs.search_Specs("numBed") + ", " + 
+							i_msg.room_specs.search_Specs("phone") + ", " + 
+							i_msg.room_specs.search_Specs("internet") + ", " + 
+							i_msg.room_specs.search_Specs("kitchen") + ", " + 
+							i_msg.cleaned + ")");
+					if (insertStatus == 1) {
+						i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room created in database.");
+						
+						reply = i_msg;
+					} else {
+						i_msg.fill_Header_Response(Header.Response.FAIL, "Insert operation failed.");
+						
+						reply = i_msg;
+					}
 				}
 			}
 		} catch (SQLException e) {
@@ -69,40 +73,44 @@ public class Room {
 		RoomMessage reply=null;
 		databaseHelper 	dbcon = null;
 		try {
-			dbcon = new databaseHelper(dbname);
-			
-			ResultSet rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-			if (!rs.next()) {
-				i_msg.fill_Header_Response(Header.Response.FAIL, "Room number is not in database.");
-				
-				reply	= i_msg;
-			} else if (i_msg.room_type == ""){
+			if (validateParam(i_msg)){
 				i_msg.fill_Header_Response(Header.Response.FAIL, "Invalid input(s).");
 				
 				reply	= i_msg;
 			} else {
-				int updateStatus = dbcon.modify("UPDATE test_rooms SET " +
-					"roomNumber=" 	+ i_msg.room_number + ", " + 
-					"roomFloor=" 	+ i_msg.room_floor + ", " + 
-					"roomType='" 	+ i_msg.room_type + "', " + 
-					"price=" 		+ i_msg.price + ", " + 
-					"onsuite=" 		+ i_msg.room_specs.search_Specs("onsuite") + ", " + 
-					"tv=" 			+ i_msg.room_specs.search_Specs("tv") + ", " + 
-					"disability=" 	+ i_msg.room_specs.search_Specs("disability") + ", " + 
-					"elevator=" 	+ i_msg.room_specs.search_Specs("elevator") + ", " + 
-					"ebirdcall=" 	+ i_msg.room_specs.search_Specs("ebirdcall") + ", " + 
-					"emornpaper=" 	+ i_msg.room_specs.search_Specs("emornpaper") + ", " + 
-					"availability=" + ((i_msg.available)?1:0) + ", " + 
-					"numBed=" 		+ i_msg.room_specs.search_Specs("numBed") + " " + 
-					"WHERE roomID=" + i_msg.room_id);
-				if (updateStatus ==1) {
-					i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room updated in database.");
+				dbcon = new databaseHelper(dbname);
+				Header tempHead = i_msg.return_Header();
+				ResultSet rs = dbcon.select("SELECT * FROM " + tempHead.name_hotel + "_rooms WHERE roomNumber = " + i_msg.room_number);
+				if (!rs.next()) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Room number is not in database.");
 					
-					reply = i_msg;
+					reply	= i_msg;
 				} else {
-					i_msg.fill_Header_Response(Header.Response.FAIL, "Update operation failed.");
-					
-					reply = i_msg;
+					int updateStatus = dbcon.modify("UPDATE test_rooms SET " +
+						"roomNumber=" 	+ i_msg.room_number + ", " + 
+						"roomFloor=" 	+ i_msg.room_floor + ", " + 
+						"roomType='" 	+ i_msg.room_type + "', " + 
+						"price=" 		+ i_msg.price + ", " + 
+						"onsuite=" 		+ i_msg.room_specs.search_Specs("onsuite") + ", " + 
+						"tv=" 			+ i_msg.room_specs.search_Specs("tv") + ", " + 
+						"disability=" 	+ i_msg.room_specs.search_Specs("disability") + ", " + 
+						"elevator=" 	+ i_msg.room_specs.search_Specs("elevator") + ", " + 
+						"availability=" + ((i_msg.available)?1:0) + ", " + 
+						"numBed=" 		+ i_msg.room_specs.search_Specs("numBed") + ", " + 
+						"phone="		+ i_msg.room_specs.search_Specs("phone") + ", " + 
+						"internet="		+ i_msg.room_specs.search_Specs("internet") + ", " + 
+						"kitchen=" 		+ i_msg.room_specs.search_Specs("kitchen") + ", " + 
+						"clean="		+ i_msg.cleaned + " " + 
+						"WHERE roomID=" + i_msg.room_id);
+					if (updateStatus ==1) {
+						i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room updated in database.");
+						
+						reply = i_msg;
+					} else {
+						i_msg.fill_Header_Response(Header.Response.FAIL, "Update operation failed.");
+						
+						reply = i_msg;
+					}
 				}
 			}
 		} catch (SQLException e) {
@@ -125,24 +133,31 @@ public class Room {
 		RoomMessage reply=null;
 		databaseHelper 	dbcon = null;
 		try {
-			dbcon = new databaseHelper(dbname);
-			
-			ResultSet rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-			if (!rs.next()) {
-				i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
+			if (i_msg.room_number < 0) {
+				i_msg.fill_Header_Response(Header.Response.FAIL, "Invalid input(s).");
 				
 				reply	= i_msg;
 			} else {
-				int deleteStatus = dbcon.modify("DELETE FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-				
-				if (deleteStatus == 1) {
-					i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room deleted from database.");
+				dbcon = new databaseHelper(dbname);
+				Header tempHead = i_msg.return_Header();
+				ResultSet rs = dbcon.select("SELECT * FROM " + tempHead.name_hotel + "_rooms WHERE roomNumber = " + i_msg.room_number);
+
+				if (!rs.next()) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
 					
-					reply = i_msg;
+					reply	= i_msg;
 				} else {
-					i_msg.fill_Header_Response(Header.Response.FAIL, "Delete operation failed.");
+					int deleteStatus = dbcon.modify("DELETE FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
 					
-					reply = i_msg;
+					if (deleteStatus == 1) {
+						i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room deleted from database.");
+						
+						reply = i_msg;
+					} else {
+						i_msg.fill_Header_Response(Header.Response.FAIL, "Delete operation failed.");
+						
+						reply = i_msg;
+					}
 				}
 			}
 		} catch (SQLException e) {
@@ -165,31 +180,37 @@ public class Room {
 		RoomMessage reply=null;
 		databaseHelper 	dbcon = null;
 		try {
-			dbcon = new databaseHelper(dbname);
-			
-			ResultSet rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-			if (!rs.next()) {
-				i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
+			if (i_msg.room_number < 0){
+				i_msg.fill_Header_Response(Header.Response.FAIL, "Invalid input(s).");
 				
 				reply	= i_msg;
 			} else {
-				rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
-				while (rs.next()) {
-					i_msg.room_id = rs.getInt("roomID");
-					i_msg.room_number = rs.getInt("roomNumber");
-					i_msg.room_floor = rs.getInt("roomFloor");
-					i_msg.room_type = rs.getString("roomType");
-					i_msg.price = rs.getFloat("price");
-					i_msg.available = rs.getBoolean("availability");
-					//i_msg.cleaned = rs.getBoolean("cleaned");
-					i_msg.room_specs.fill_Specs(rs.getBoolean("onsuite"), rs.getBoolean("tv"), rs.getBoolean("disability"), 
-							rs.getBoolean("elevator"), rs.getBoolean("ebirdcall"), rs.getBoolean("emornpaper"), 
-							(Integer)rs.getInt("numBed"));
+				dbcon = new databaseHelper(dbname);
+				Header tempHead = i_msg.return_Header();
+				ResultSet rs = dbcon.select("SELECT * FROM " + tempHead.name_hotel + "_rooms WHERE roomNumber = " + i_msg.room_number);
+				if (!rs.next()) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
+					
+					reply	= i_msg;
+				} else {
+					rs = dbcon.select("SELECT * FROM test_rooms WHERE roomNumber = " + i_msg.room_number);
+					while (rs.next()) {
+						i_msg.room_id = rs.getInt("roomID");
+						i_msg.room_number = rs.getInt("roomNumber");
+						i_msg.room_floor = rs.getInt("roomFloor");
+						i_msg.room_type = rs.getString("roomType");
+						i_msg.price = rs.getFloat("price");
+						i_msg.available = rs.getBoolean("availability");
+						i_msg.cleaned = rs.getBoolean("clean");
+						i_msg.room_specs.fill_Specs(rs.getBoolean("onsuite"), rs.getBoolean("tv"), rs.getBoolean("disability"), 
+								rs.getBoolean("elevator"), (Integer)rs.getInt("numBed"), rs.getBoolean("phone"), 
+								rs.getBoolean("internet"), rs.getBoolean("kitchen"));
+					}
+					
+					i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room retrieved from database.");
+					
+					reply = i_msg;
 				}
-				
-				i_msg.fill_Header_Response(Header.Response.SUCCESS, "Room retrieved from database.");
-				
-				reply = i_msg;
 			}
 		} catch (SQLException e) {
 			System.err.println("Error in 'editRoom'.  SQLException was thrown:");
@@ -214,33 +235,40 @@ public class Room {
 		RoomMessage tempMsg = null;
 		int i = 0;
 		try {
-			dbcon = new databaseHelper(dbname);
-			
-			ResultSet rs = dbcon.select("SELECT * FROM test_rooms");
-			if (!rs.next()) {
-				i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
-				output[0] = i_msg.deepCopy();
-				i++;
+			if (i_msg.room_number < 0){
+				i_msg.fill_Header_Response(Header.Response.FAIL, "Invalid input(s).");
+				
+				reply	= i_msg;
 			} else {
-				while (rs.next()) {
+				dbcon = new databaseHelper(dbname);
+				Header tempHead = i_msg.return_Header();
+				ResultSet rs = dbcon.select("SELECT * FROM " + tempHead.name_hotel + "_rooms");
+				if (!rs.next()) {
+					i_msg.fill_Header_Response(Header.Response.FAIL, "Room number not in database.");
+					output[0] = i_msg.deepCopy();
 					i++;
-				}
-				rs = dbcon.select("SELECT * FROM test_rooms");
-				output = new RoomMessage[i];
-				i = 0;
-				tempMsg = i_msg.deepCopy();
-				while (rs.next()) {
-					tempMsg.room_number = rs.getInt("num");
-					tempMsg.room_floor = rs.getInt("floor");
-					tempMsg.room_type = rs.getString("type");
-					tempMsg.price = rs.getFloat("price");
-					tempMsg.available = rs.getBoolean("availability");
-					tempMsg.available = rs.getBoolean("cleaned");
-					tempMsg.room_specs.fill_Specs(rs.getBoolean("onsuite"), rs.getBoolean("tv"), rs.getBoolean("disability"), 
-							rs.getBoolean("elevator"), rs.getBoolean("ebirdcall"), rs.getBoolean("emornpaper"), 
-							rs.getInt("numBed"));
-					tempMsg.fill_Header_Response(Header.Response.SUCCESS, "Room retrieved from database.");
-					output[i++] = tempMsg.deepCopy();
+				} else {
+					while (rs.next()) {
+						i++;
+					}
+					rs = dbcon.select("SELECT * FROM test_rooms");
+					output = new RoomMessage[i];
+					i = 0;
+					tempMsg = i_msg.deepCopy();
+					while (rs.next()) {
+						tempMsg.room_id = rs.getInt("roomID");
+						tempMsg.room_number = rs.getInt("roomNumber");
+						tempMsg.room_floor = rs.getInt("roomFloor");
+						tempMsg.room_type = rs.getString("roomType");
+						tempMsg.price = rs.getFloat("price");
+						tempMsg.available = rs.getBoolean("availability");
+						tempMsg.cleaned = rs.getBoolean("cleaned");
+						tempMsg.room_specs.fill_Specs(rs.getBoolean("onsuite"), rs.getBoolean("tv"), rs.getBoolean("disability"), 
+								rs.getBoolean("elevator"), (Integer)rs.getInt("numBed"), rs.getBoolean("phone"), 
+								rs.getBoolean("internet"), rs.getBoolean("kitchen"));
+						tempMsg.fill_Header_Response(Header.Response.SUCCESS, "Rooms retrieved from database.");
+						output[i++] = tempMsg.deepCopy();
+					}
 				}
 			}
 		} catch (SQLException e) {
@@ -258,5 +286,17 @@ public class Room {
 			if (dbcon != null) dbcon.close();
 		}	
 		return output;
+	}
+	
+	private boolean validateParam(RoomMessage i_msg){
+		boolean validity = true;
+		
+		if (i_msg.room_id < 0 || i_msg.room_floor < 0 || i_msg.room_floor < 0 || i_msg.price < 0 ||
+				(Integer)i_msg.room_specs.search_Specs("numBed") < 0) {
+			validity = false;		
+		} if (i_msg.room_type == "" || i_msg.room_type == null) {
+			validity = false;
+		}
+		return validity;
 	}
 }
