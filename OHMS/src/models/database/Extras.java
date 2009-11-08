ec6345c4196ee887eb3c27af08fbd9d173d9a9b5
willennium615 <willennium615@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-08 01:42:51 +0000

    extras add,edit,delete,view should be done properly with tests done...

--- OHMS/src/models/database/Extras.java
+++ OHMS/src/models/database/Extras.java
@@ -15,14 +15,18 @@ public class Extras {
 			dbcon 				= new databaseHelper(iheader.nameHotel);
 			java.util.Date today 	= new java.util.Date();
 			java.sql.Date now = new java.sql.Date(today.getTime());
-			int returnedRows = dbcon.modify("INSERT INTO extra (orderDate, extraName, extraCost, bookingID) VALUES ('" 
-					+ now
+			int returnedRows = dbcon.modify("INSERT INTO test_extras (extraID, bookingID, description, price, orderDate) VALUES ('" 
+					+ i_msg.extraID + "', '"  
+					+ i_msg.bookingID + "', '" 
 					+ i_msg.extraName + "', '" 
 					+ i_msg.extraCost + "', '" 
-					+ i_msg.bookingID + "')");
-			if (returnedRows == 1) {
-				output.fillHeaderResponse(Header.Response.SUCCESS, "Added one extra Requested.");
+					+ now + "')");
+			if (returnedRows > 0) {
+				System.out.println("Success");
+				output.fillHeaderResponse(Header.Response.SUCCESS, "Added one extra as Requested.");
+				output.extraID=returnedRows;
 			} else {
+				System.out.println("Failure");
 				output.fillHeaderResponse(Header.Response.FAIL, "Adding extra failed.");
 			}
 			
@@ -50,7 +54,7 @@ public class Extras {
 		try {
 			dbcon 				= new databaseHelper(iheader.nameHotel);
 
-			int returnedRows = dbcon.modify("DELETE FROM extra WHERE bookingID='" + i_msg.bookingID + "'");
+			int returnedRows = dbcon.modify("DELETE FROM test_extras WHERE extraID='" + i_msg.extraID + "'");
 			if (returnedRows == 1) {
 				output.fillHeaderResponse(Header.Response.SUCCESS, "Delete one extra as Requested.");
 			} else {
@@ -81,14 +85,15 @@ public class Extras {
 		try {
 			dbcon 				= new databaseHelper(iheader.nameHotel);
 					
-			ResultSet rs=dbcon.select("Select * FROM extra WHERE bookingID='" + i_msg.bookingID 
-					+ " AND extraName='" + i_msg.extraName +"'");
+			ResultSet rs=dbcon.select("Select * FROM test_extras WHERE extraID='" + i_msg.extraID 
+					+"'");
 			while (rs.next()) {
+				int cextraID = rs.getInt("extraID");
 				int cbookingID = rs.getInt("bookingID");
 				Date corderDate = rs.getDate("orderDate");
-				String cextraName = rs.getString("extraName");
-				float cextraCost = rs.getFloat("extraCost");
-				output.fillAll(corderDate, cbookingID, cextraName, cextraCost);	      
+				String cextraName = rs.getString("description");
+				float cextraCost = rs.getFloat("price");
+				output.fillAll(cextraID, cbookingID, cextraName, cextraCost, corderDate);      
 	            }
 			output.fillHeaderResponse(Header.Response.SUCCESS, "View one extra as Requested.");
 		} catch (SQLException e) {
@@ -115,8 +120,9 @@ public class Extras {
 		try {
 			dbcon 				= new databaseHelper(iheader.nameHotel);
 
-			int returnedRows = dbcon.modify("UPDATE extra SET extraName='" + i_msg.extraName
-					+ "', extraCost='" + i_msg.extraCost +  "'");
+			int returnedRows = dbcon.modify("UPDATE test_extras SET description='" + i_msg.extraName
+					+ "', price='" + i_msg.extraCost +  "'" + "WHERE extraID='" + i_msg.extraID
+					+ "'");
 			if (returnedRows == 1) {
 				output.fillHeaderResponse(Header.Response.SUCCESS, "Edited one extra as Requested.");
 			} else {
@@ -149,14 +155,14 @@ public class Extras {
 				dbcon 				= new databaseHelper(iheader.nameHotel);
 				// booking id or owner + sDate
 				ResultSet rs=dbcon.select("Select count(*) FROM " + iheader.nameHotel + 
-										"_extra WHERE bookingID='" + i_msg.bookingID +"'");
+										"_extra WHERE extraID='" + i_msg.extraID +"'");
 				int numberofrows=rs.getInt(1);
 				if(numberofrows<1){
 					output[0].fillHeaderResponse(Header.Response.FAIL, "viewAll Booking failed." +
-							" bookingID: " + i_msg.bookingID);
+							" extraID: " + i_msg.extraID);
 					return output;
 				}
-				rs=dbcon.select("Select * FROM extra WHERE bookingID='" + i_msg.bookingID + "'");
+				rs=dbcon.select("Select * FROM extra WHERE extraID='" + i_msg.extraID + "'");
 				int i=0;
 				while (rs.next()) {
 					output[i]=new ExtraMessage(iheader.messageOwnerID, iheader.authLevel, iheader.nameHotel, iheader.action);
@@ -165,17 +171,17 @@ public class Extras {
 					output[i].extraCost=rs.getFloat("extraCost");
 					output[i].orderDate= rs.getDate("orderDate");
 					
-					output[i].fillHeaderResponse(Header.Response.SUCCESS, "ViewAll one Booking as Requested.");
+					output[i].fillHeaderResponse(Header.Response.SUCCESS, "ViewAll one extra as Requested.");
 					i++;
 		        }
 			} catch (SQLException e) {
 				System.err.println("Error in 'Add_Account'.  SQLException was thrown:");
 				e.printStackTrace(System.err);
-				output[0].fillHeaderResponse(Header.Response.FAIL, "view Booking failed.");
+				output[0].fillHeaderResponse(Header.Response.FAIL, "view extra failed.");
 			} catch (ClassNotFoundException e) {
 				System.err.println("Error in 'Add_Account'.  ClassNotFoundException was thrown:");
 				e.printStackTrace(System.err);
-				output[0].fillHeaderResponse(Header.Response.FAIL, "view Booking failed.");
+				output[0].fillHeaderResponse(Header.Response.FAIL, "view extra failed.");
 			}
 			finally {
 				if (dbcon != null) {
--- OHMS/src/testMains/ExtrasTest.java
+++ OHMS/src/testMains/ExtrasTest.java
@@ -7,44 +7,40 @@ import models.messages.Header;
 public class ExtrasTest {
 	public static void main(String [ ] args){
 		System.out.println("\r");
-		test_Extras(0);
+		//test_Extras(0);
+		//test_Extras(1);
+		test_Extras(2);
+		//test_Extras(3);
 	}
 	
 		private static void test_Extras(int i_num){	
 			if (i_num==0){
 				System.out.println("Start Add Extra Test");
-				ExtraMessage h_msg = new ExtraMessage(0, 0, "test", Header.Action.ADD);
+				ExtraMessage h_msg = new ExtraMessage(1, 1, "test", Header.Action.ADD);
 				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
-				h_msg.fillAll(date,4, "TV", 700);
+				h_msg.fillAll(2 , 4, "TV", 700,date);
 				Hotel hotel = new Hotel("test");
 				hotel.processMessage(h_msg);
-				Header head=h_msg.returnHeader();
-				
+				Header head=h_msg.returnHeader();				
 				//ExtraMessage reply=(ExtraMessage) hotel.processMessage(h_msg);
 				//Header head=reply.returnHeader();
 				if(Header.Response.SUCCESS==head.responseCode){
 				System.out.println("Passed Test ID 1");
-				System.out.println("\r");
+				
 			}
 			else{
 				System.out.println("Failed Test ID 1");
-				System.out.println("\r");
 			}
-				System.out.println("Finish Add Extra Test");		
+				System.out.println("Finish Add Extra Test");
 		}
 			if (i_num==1){
 				System.out.println("Start Delete Extra Test");
-				ExtraMessage h_msg = new ExtraMessage(0, 0, "test", Header.Action.ADD);
-				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
-				h_msg.fillAll(date, 5, "TV", 700);
 				Hotel hotel = new Hotel("test");
-				hotel.processMessage(h_msg);
-				
 				ExtraMessage g_msg = new ExtraMessage(0, 0, "test", Header.Action.DELETE);
-				java.sql.Date date1 = new java.sql.Date(new java.util.Date().getTime());
-				h_msg.fillAll(date1, 5, "TV", 700);
+				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+				g_msg.fillAll(2, 5, "TV", 700,date);
 				hotel.processMessage(g_msg);
-				Header head=h_msg.returnHeader();
+				Header head=g_msg.returnHeader();
 				if(Header.Response.SUCCESS==head.responseCode){
 				System.out.println("Passed Test ID 2");
 				System.out.println("\r");
@@ -53,7 +49,70 @@ public class ExtrasTest {
 				System.out.println("Failed Test ID 2");
 				System.out.println("\r");
 				}
+				System.out.println("Finish Delete extra Test");
 			}
-			System.out.println("Finish Delete extra Test");
+			
+			if(i_num==2){
+				System.out.println("Start Edit Extra Test");
+				ExtraMessage h_msg = new ExtraMessage(1, 1, "test", Header.Action.EDIT);
+				h_msg.extraID=2;
+				h_msg.extraName="beer";
+				h_msg.extraCost=1000;
+				Hotel hotel = new Hotel("test");
+				hotel.processMessage(h_msg);
+				Header head=h_msg.returnHeader();				
+				//ExtraMessage reply=(ExtraMessage) hotel.processMessage(h_msg);
+				//Header head=reply.returnHeader();
+				if(Header.Response.SUCCESS==head.responseCode){
+				System.out.println("Passed Test ID 1");
+				
+			}
+			else{
+				System.out.println("Failed Test ID 1");
+			}
+				System.out.println("Finish Edit Extra Test");
+			}
+			
+			if(i_num==3){
+				System.out.println("Start View Extra Test");
+				Hotel hotel = new Hotel("test");
+				ExtraMessage g_msg = new ExtraMessage(0, 0, "test", Header.Action.VIEW);
+				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+				g_msg.extraID=2;
+				
+				hotel.processMessage(g_msg);
+				Header head=g_msg.returnHeader();
+				if(Header.Response.SUCCESS==head.responseCode){
+				System.out.println("Passed Test ID 2");
+				System.out.println(head.responseCode);
+				System.out.println(head.responseString);
+				System.out.println("\r");
+				}
+			else{
+				System.out.println("Failed Test ID 2");
+				System.out.println("\r");
+				}
+				System.out.println("Finish View extra Test");
+			}
 		}
 }
+
+/*
+ 				System.out.println("Start Running Test for Add booking");
+				Hotel hotel = new Hotel("test");
+				ExtraMessage input=null;
+				ExtraMessage reply=null;
+				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());			
+			    input= new ExtraMessage(1, 1, "test", Header.Action.ADD);
+			    input.extraID=1;
+			    input.bookingID=1;
+			    input.extraName="TV";
+			    input.extraCost=700;
+				input.orderDate=date;
+				reply= (ExtraMessage) hotel.processMessage(input);
+				Header head = reply.returnHeader();
+				System.out.println("...");
+				System.out.println(head.responseCode);
+				System.out.println(head.responseString);
+				System.out.println("....");
+*/
