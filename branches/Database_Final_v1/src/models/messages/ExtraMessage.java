fcc221dad66ad790e4369e75c0e1bf496252d416
willennium615 <willennium615@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-18 21:21:06 +0000

    updated fillAll and printAll

--- branches/Database_Final_v1/src/models/messages/ExtraMessage.java
+++ branches/Database_Final_v1/src/models/messages/ExtraMessage.java
@@ -1,5 +1,7 @@
 package models.messages;
 
+import java.util.Date;
+
 public class ExtraMessage{
 	// attributes
 	public int 					extraID;
@@ -8,4 +10,24 @@ public class ExtraMessage{
 	public float				price;
 	public java.sql.Timestamp	date;
 	public java.sql.Timestamp	creationDate;
+	
+	public void fillAll(int eID, int bID, String eName, float ePrice, java.sql.Timestamp eDate, java.sql.Timestamp cDate ){
+		extraID=eID;
+		bookingID=bID;
+		extraName=eName;
+		price=ePrice;
+		date=eDate;
+		creationDate=cDate;		
+	}
+	
+	public void print_All(){
+		System.out.println("Extras Message is printing out");
+		System.out.println(extraID);
+		System.out.println(bookingID);
+		System.out.println(extraName);
+		System.out.println(price);
+		System.out.println(date);
+		System.out.println(creationDate);
+		System.out.println("*******************");
+	}
 }
