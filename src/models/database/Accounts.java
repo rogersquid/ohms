6601c55d32d435343a93e19b0443b7d7b7ab4d3f
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-18 09:07:27 +0000

    Skeleton of Account - Michael and Raj will fill it up soon

---
+++ src/models/database/Accounts.java
@@ -0,0 +1,14 @@
+package models.database;
+import models.messages.*;
+import java.util.regex.Matcher;
+public class Accounts {
+	
+	public Message deleteAccount(Message i_msg)
+	{
+		Message r_i_msg = i_msg;
+		
+		return r_i_msg;
+	}
+	
+	
+}
