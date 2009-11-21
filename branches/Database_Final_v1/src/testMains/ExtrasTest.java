edc5d939eb459cf62bfb684e0729a6f585a7aee1
willennium615 <willennium615@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-21 04:46:38 +0000

    extra tests updated...waiting for verify params to be done by Raj to fully test

--- branches/Database_Final_v1/src/testMains/ExtrasTest.java
+++ branches/Database_Final_v1/src/testMains/ExtrasTest.java
@@ -1,118 +1,359 @@
 package testMains;
 
-import models.database.Hotel;
-import models.messages.ExtraMessage;
-import models.messages.Header;
+import models.database.*;
+import models.messages.*;
+
 
 public class ExtrasTest {
 	public static void main(String [ ] args){
 		System.out.println("\r");
-		//test_Extras(0);
-		//test_Extras(1);
-		test_Extras(2);
-		//test_Extras(3);
+		test_allExtra();
 	}
 	
-		private static void test_Extras(int i_num){	
-			if (i_num==0){
-				System.out.println("Start Add Extra Test");
-				ExtraMessage h_msg = new ExtraMessage(1, 1, "test", Header.Action.ADD);
-				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
-				h_msg.fillAll(2 , 4, "TV", 700,date);
-				Hotel hotel = new Hotel("test");
-				hotel.processMessage(h_msg);
-				Header head=h_msg.returnHeader();				
-				//ExtraMessage reply=(ExtraMessage) hotel.processMessage(h_msg);
-				//Header head=reply.returnHeader();
-				if(Header.Response.SUCCESS==head.responseCode){
-				System.out.println("Passed Test ID 1");
-				
-			}
-			else{
-				System.out.println("Failed Test ID 1");
-			}
-				System.out.println("Finish Add Extra Test");
+	public static void test_allExtra(){
+		System.out.println("Start Test Extras \r");
+		for (int i=0; i<1; i++){
+			test_addExtra(i);
 		}
-			if (i_num==1){
-				System.out.println("Start Delete Extra Test");
-				Hotel hotel = new Hotel("test");
-				ExtraMessage g_msg = new ExtraMessage(0, 0, "test", Header.Action.DELETE);
-				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
-				g_msg.fillAll(2, 5, "TV", 700,date);
-				hotel.processMessage(g_msg);
-				Header head=g_msg.returnHeader();
-				if(Header.Response.SUCCESS==head.responseCode){
-				System.out.println("Passed Test ID 2");
-				System.out.println("\r");
-				}
-			else{
-				System.out.println("Failed Test ID 2");
-				System.out.println("\r");
-				}
-				System.out.println("Finish Delete extra Test");
-			}
-			
-			if(i_num==2){
-				System.out.println("Start Edit Extra Test");
-				ExtraMessage h_msg = new ExtraMessage(1, 1, "test", Header.Action.EDIT);
-				h_msg.extraID=2;
-				h_msg.extraName="beer";
-				h_msg.extraCost=1000;
-				Hotel hotel = new Hotel("test");
-				hotel.processMessage(h_msg);
-				Header head=h_msg.returnHeader();				
-				//ExtraMessage reply=(ExtraMessage) hotel.processMessage(h_msg);
-				//Header head=reply.returnHeader();
-				if(Header.Response.SUCCESS==head.responseCode){
-				System.out.println("Passed Test ID 1");
-				
-			}
-			else{
-				System.out.println("Failed Test ID 1");
-			}
-				System.out.println("Finish Edit Extra Test");
-			}
-			
-			if(i_num==3){
-				System.out.println("Start View Extra Test");
-				Hotel hotel = new Hotel("test");
-				ExtraMessage g_msg = new ExtraMessage(0, 0, "test", Header.Action.VIEW);
-				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
-				g_msg.extraID=2;
-				
-				hotel.processMessage(g_msg);
-				Header head=g_msg.returnHeader();
-				if(Header.Response.SUCCESS==head.responseCode){
-				System.out.println("Passed Test ID 2");
-				System.out.println(head.responseCode);
-				System.out.println(head.responseString);
-				System.out.println("\r");
-				}
-			else{
-				System.out.println("Failed Test ID 2");
-				System.out.println("\r");
-				}
-				System.out.println("Finish View extra Test");
-			}
+		for (int j=0; j<1; j++){
+			test_editExtra(j);
 		}
-}
+		for (int k=0; k<1; k++){
+			test_deleteExtra(k);
+		}
+		for (int l=0; l<1; l++){
+			test_viewExtra(l);
+		}
+		for (int m=0; m<1; m++){
+			test_viewAllExtra(m);
+		}
+		
+		System.out.println("Finish Test Extras \r");
+	}
+	
+	private static void test_addExtra (int i_num){
+		
+		//=====================Extras Tests==========================
+		
+		if(i_num==0){
+			System.out.println("Test ID 1");
+			System.out.println("Description: Success Added Extra");
 
-/*
- 				System.out.println("Start Running Test for Add booking");
-				Hotel hotel = new Hotel("test");
-				ExtraMessage input=null;
-				ExtraMessage reply=null;
-				java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());			
-			    input= new ExtraMessage(1, 1, "test", Header.Action.ADD);
-			    input.extraID=1;
-			    input.bookingID=1;
-			    input.extraName="TV";
-			    input.extraCost=700;
-				input.orderDate=date;
-				reply= (ExtraMessage) hotel.processMessage(input);
-				Header head = reply.returnHeader();
-				System.out.println("...");
-				System.out.println(head.responseCode);
-				System.out.println(head.responseString);
-				System.out.println("....");
-*/
+			//Filling Header information
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(1);
+			h_msg.extras[0] = new ExtraMessage();
+			//fill Extra info:
+			java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+			java.sql.Timestamp creationDate=new java.sql.Timestamp(new java.util.Date().getTime());
+			h_msg.extras[0].fillAll(1, 2, "Beer", 10, date, creationDate);
+			//h_msg.response = h_msg.Extras[0].validateParam(h_msg);
+			if (h_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+				Extra Extra = new Extra();
+				h_msg = Extra.addExtra(h_msg);
+			}
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				System.out.println("Passed Test ID 1");
+				System.out.println("Input:");
+				System.out.println("Output: " + h_msg.response.responseString);
+				System.out.println("\r");
+			} else{
+				System.out.println("Failed Test ID 1");
+				System.out.println("Expected Output:");
+				System.out.println("Expected Change:");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}		
+	}	
+
+
+	private static void test_editExtra(int i_num){
+		java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+		java.sql.Timestamp creationDate=new java.sql.Timestamp(new java.util.Date().getTime());
+		//=====================Edit Extra Tests==========================
+		if(i_num==0){
+			System.out.println("Test ID 1");
+			System.out.println("Description: Success Edit Extra: Price");
+			//Filling Header information
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(1);
+			h_msg.extras[0] = new ExtraMessage();
+			//fill Extra info:
+			h_msg.extras[0].fillAll(1, 2, "Beer", 1000, date, creationDate);
+			
+			Extra Extra = new Extra();
+			//h_msg.response = h_msg.extras[0].validateParam(h_msg);
+			if (h_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+				h_msg = Extra.addExtra(h_msg);
+			}
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				Message g_msg= new Message(0, 0, "test");
+				g_msg.initializeExtras(1);
+				g_msg.extras[0] = new ExtraMessage();
+				//fill Extra info:
+				//test_fillExtra(g_msg, t_set, i_num);
+				g_msg.extras[0].extraID = h_msg.extras[0].extraID;
+				g_msg.extras[0].price = (float)199.99;
+				//g_msg.response = h_msg.extras[0].validateParam(h_msg);
+				if (g_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+					g_msg = Extra.editExtra(g_msg);
+				}
+				if(g_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+					System.out.println("Passed Test ID 1");
+					System.out.println("Input:");
+					System.out.println("Output: " + g_msg.response.responseString);
+					System.out.println("\r");
+				}
+				else{
+					System.out.println("Failed Test ID 1");
+					System.out.println("Expected Output:");
+					System.out.println("Expected Change:");
+					System.out.println("Error Message: " + g_msg.response.responseString);
+					System.out.println("\r");
+					}
+				}
+			else{
+				System.out.println("Failed Test ID 1 at verify");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}
+	}
+
+	private static void test_deleteExtra (int i_num){
+		java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+		java.sql.Timestamp creationDate=new java.sql.Timestamp(new java.util.Date().getTime());
+		//=====================Delete Tests==========================
+		if(i_num==0){
+			System.out.println("Test ID 1");
+			System.out.println("Description: Success Delete Extra");
+			//create message
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(1);
+			h_msg.extras[0] = new ExtraMessage();
+			//fill Extra info:
+			h_msg.extras[0].fillAll(1, 2, "Beer", 1000, date, creationDate);
+			Extra Extra = new Extra();
+			//h_msg.response = h_msg.extras[0].validateParam(h_msg);
+			if (h_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+				h_msg = Extra.addExtra(h_msg);
+			} 
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				Message g_msg= new Message(0, 0, "test");
+				g_msg.initializeExtras(1);
+				g_msg.extras[0] = new ExtraMessage();
+				//fill Extra info:
+				h_msg.extras[0].fillAll(1, 2, "Beer", 1000, date, creationDate);
+				g_msg.extras[0].extraID = h_msg.extras[0].extraID;
+				g_msg = Extra.deleteExtra(g_msg);
+				if(g_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+					System.out.println("Passed Test ID 1");
+					System.out.println("Input:");
+					System.out.println("Output: " + g_msg.response.responseString);
+					System.out.println("\r");
+				}
+				else{
+					System.out.println("Failed Test ID 1");
+					System.out.println("Expected Output:");
+					System.out.println("Expected Change:");
+					System.out.println("Error Message: " + g_msg.response.responseString);
+					System.out.println("\r");
+					}
+			} else{
+				System.out.println("Failed Test ID 1 at verify");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}
+		}
+
+	private static void test_viewExtra (int i_num){
+		java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+		java.sql.Timestamp creationDate=new java.sql.Timestamp(new java.util.Date().getTime());
+		//=====================View Tests==========================
+		if(i_num==0){
+			System.out.println("Test ID 1");
+			System.out.println("Description: Success View Extra");
+			System.out.println("   Test case depends on success addition of Extra 101");
+			
+			//create message
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(1);
+			h_msg.extras[0] = new ExtraMessage();
+			//fill Extra info:
+			h_msg.extras[0].fillAll(1, 2, "Beer", 1000, date, creationDate);
+			Extra Extra = new Extra();
+			h_msg = Extra.getExtra(h_msg);
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				System.out.println("Passed Test ID 1");
+				System.out.println("Input: " + h_msg.extras[0].extraID);
+				System.out.println("Output: ");
+				test_printExtra(h_msg.extras[0]);
+				
+				System.out.println("\r");
+			}
+			else{
+				System.out.println("Failed Test ID 1");
+				System.out.println("Expected Output:");
+				System.out.println("Expected Change:");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}		
+	}
+
+	private static void test_viewAllExtra (int i_num){
+		java.sql.Date date=new java.sql.Date(new java.util.Date().getTime());
+		java.sql.Timestamp creationDate=new java.sql.Timestamp(new java.util.Date().getTime());
+		//=====================View All Tests==========================
+		if(i_num==0){
+			System.out.println("Test ID 1");
+			System.out.println("Description: Success View All Extra");
+			System.out.println("   Test case depends on success addition of Extra 101");
+			
+			//create message
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(1);
+			h_msg.extras[0] = new ExtraMessage();
+			//fill Extra info:
+			h_msg.extras[0].fillAll(1, 2, "Beer", 1000, date, creationDate);
+			Extra Extra = new Extra();
+			h_msg = Extra.getAllExtra(h_msg);
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				System.out.println("Passed Test ID 1");
+				System.out.println("Input: " + h_msg.extras[0].extraID);
+				System.out.println("Output: # of Extras: " + h_msg.extras.length);
+				for (int i = 0; i < h_msg.extras.length; i ++) {
+					test_printExtra(h_msg.extras[i]);
+				}
+				
+				System.out.println("\r");
+			}
+			else{
+				System.out.println("Failed Test ID 1");
+				System.out.println("Expected Output:");
+				System.out.println("Expected Change:");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}
+
+		if(i_num==1){
+			System.out.println("Test ID 2");
+			System.out.println("Description: Success View all Extra available");
+			
+			//create message
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(2);
+			//fill filter toggle Extra Message
+			h_msg.extras[0] = new ExtraMessage();
+			h_msg.extras[0].extraID = 2;
+			h_msg.extras[0].bookingID = 3;
+			h_msg.extras[0].extraName = "Beer";
+			h_msg.extras[0].price = 10;
+			h_msg.extras[0].date = date;
+			h_msg.extras[0].creationDate = creationDate;
+			
+			//fill filter value extra Message
+			h_msg.extras[1] = new ExtraMessage();
+			h_msg.extras[1].extraID = 3;
+			h_msg.extras[1].bookingID = 40;
+			h_msg.extras[1].extraName = "Wine";
+			h_msg.extras[1].price = 100;
+			h_msg.extras[1].date = date;
+			h_msg.extras[1].creationDate = creationDate;
+			
+			Extra Extra = new Extra();
+			//h_msg.response = h_msg.extras[0].validateParam(h_msg);
+			if (h_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+				h_msg = Extra.getFilteredExtra(h_msg);
+			} 
+			
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				System.out.println("Passed Test ID 2");
+				System.out.println("Input: " + h_msg.extras[0].extraID);
+				System.out.println("Output: # of extras: " + h_msg.extras.length);
+				for (int i = 0; i < h_msg.extras.length; i ++) {
+					test_printExtra(h_msg.extras[i]);
+				}
+				
+				System.out.println("\r");
+			}
+			else{
+				System.out.println("Failed Test ID 2");
+				System.out.println("Expected Output:");
+				System.out.println("Expected Change:");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}
+
+		if(i_num==2){
+			System.out.println("Test ID 3");
+			System.out.println("Description: Success View all Single extra on the 1st floor");
+			
+			//create message
+			Message h_msg= new Message(0, 0, "test");
+			h_msg.initializeExtras(2);
+			//fill filter toggle extra Message
+			h_msg.extras[0] = new ExtraMessage();
+			h_msg.extras[0].extraID = 2;
+			h_msg.extras[0].bookingID = 3;
+			h_msg.extras[0].extraName = "Beer";
+			h_msg.extras[0].price = 10;
+			h_msg.extras[0].date = date;
+			h_msg.extras[0].creationDate = creationDate;
+			
+			//fill filter value extra Message
+			h_msg.extras[1] = new ExtraMessage();
+			h_msg.extras[1].extraID = 3;
+			h_msg.extras[1].bookingID = 40;
+			h_msg.extras[1].extraName = "Wine";
+			h_msg.extras[1].price = 100;
+			h_msg.extras[1].date = date;
+			h_msg.extras[1].creationDate = creationDate;
+			
+			Extra Extra = new Extra();
+			//h_msg.response = h_msg.extras[0].validateParam(h_msg);
+			if (h_msg.response.responseCode != ResponseMessage.ResponseCode.FAIL) {
+				h_msg = Extra.getFilteredExtra(h_msg);
+			} 
+			
+			if(h_msg.response.responseCode == ResponseMessage.ResponseCode.SUCCESS){
+				System.out.println("Passed Test ID 3");
+				System.out.println("Input: " + h_msg.extras[0].extraID);
+				System.out.println("Output: # of extras: " + h_msg.extras.length);
+				for (int i = 0; i < h_msg.extras.length; i ++) {
+					test_printExtra(h_msg.extras[i]);
+				}
+				
+				System.out.println("\r");
+			}
+			else{
+				System.out.println("Failed Test ID 3");
+				System.out.println("Expected Output:");
+				System.out.println("Expected Change:");
+				System.out.println("Error Message: " + h_msg.response.responseString);
+				System.out.println("\r");
+			}
+		}
+		
+	}
+	
+	private static void test_printExtra (ExtraMessage h_msg){
+		System.out.println("   Extra ID: " + h_msg.extraID);
+		System.out.println("   Booking ID: " + h_msg.bookingID);
+		System.out.println("   Extra Name: " + h_msg.extraName);
+		System.out.println("   Price: " + h_msg.price);
+		System.out.println("   Date: " + h_msg.date);
+		System.out.println("   Creation Date: " + h_msg.creationDate);
+
+		System.out.println("\r");
+	}
+}
