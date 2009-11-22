8f876ab49470aa4c46868e743a5984321dc40f8a
mayfer <mayfer@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-22 11:45:02 +0000

    register is bomb

--- WEB-INF/classes/controllers/registerServlet.java
+++ WEB-INF/classes/controllers/registerServlet.java
@@ -19,37 +19,50 @@ public class registerServlet extends HttpServlet {
 	throws IOException, ServletException
 	{
 		String password = request.getParameter("password");
+		String re_password = request.getParameter("re_password");
 		String firstname = request.getParameter("firstname");
 		String lastname = request.getParameter("lastname");
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
 		String address = request.getParameter("address");
-		boolean gender = (request.getParameter("gender") == "1") ? true : false;
+		boolean gender = (request.getParameter("gender").equals("0")) ? false : true;
 
-		int userid = 1;
+		int userid = 6;
 		int authlevel = 3;
 		String hotelname = "test";
-		AccountMessage message = new AccountMessage(userid, authlevel, hotelname, Header.Action.ADD);
-		message.email = email;
-		message.password = password;
-		message.firstname = firstname;
-		message.lastname = lastname;
-		message.phone = phone;
-		message.address = address;
-		message.gender = gender;
+		Message message = new Message(authlevel, userid, hotelname);
+		message.initializeAccounts(1);
+		message.accounts[0].email = email;
+		message.accounts[0].password = password;
+		message.accounts[0].re_password = re_password;
+		message.accounts[0].firstName = firstname;
+		message.accounts[0].lastName = lastname;
+		message.accounts[0].phone = phone;
+		message.accounts[0].address = address;
+		message.accounts[0].gender = gender;
 
-		Hotel hotel = new Hotel(hotelname);
-		AccountMessage reply = (AccountMessage)hotel.processMessage(message);
-		Header replyHeader = reply.returnHeader();
+		request.setAttribute("account", message.accounts[0]);
 
-		if(replyHeader.response_code == Header.Response.SUCCESS) {
-			request.setAttribute("firstname", firstname);
-			request.setAttribute("lastname", lastname);
-			getServletContext().getRequestDispatcher("/views/register_success.jsp").include(request, response);
+		if(message.validate()) {
+			if(message.response.responseCode == ResponseMessage.ResponseCode.SUCCESS) {
+				Account account = new Account();
+				Message reply = account.addAccount(message);
+
+				if(reply.response.responseCode == ResponseMessage.ResponseCode.SUCCESS) {
+					getServletContext().getRequestDispatcher("/views/register_success.jsp").include(request, response);
+				} else {
+					request.setAttribute("status", "register_failed");
+					request.setAttribute("message", reply.response.responseString);
+					getServletContext().getRequestDispatcher("/views/register_form.jsp").include(request, response);
+				}
+			} else {
+				request.setAttribute("status", "register_failed");
+				request.setAttribute("message", message.response.responseString);
+				getServletContext().getRequestDispatcher("/views/register_form.jsp").include(request, response);
+			}
 		} else {
-			request.setAttribute("status", "register_failed");
-			request.setAttribute("message", replyHeader.response_string);
-			getServletContext().getRequestDispatcher("/views/register_form.jsp").include(request, response);
+			request.setAttribute("message", "Message validation failed, all message objects are null.");
+			getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
 		}
 	}
 
