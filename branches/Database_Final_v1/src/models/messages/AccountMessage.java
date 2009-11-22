cf95335c783324e56711af2c077912e83501cc4f
mayfer <mayfer@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-22 11:46:18 +0000

    register functions work perfectly, including validations

--- branches/Database_Final_v1/src/models/messages/AccountMessage.java
+++ branches/Database_Final_v1/src/models/messages/AccountMessage.java
@@ -9,12 +9,13 @@ public class AccountMessage{
 	public String firstName;
 	public String lastName;
 	public String password;
+	public String re_password;
 	public String phone;
 	public String address;
 	public String email;
 	public boolean gender;
 	public java.sql.Timestamp creationTime;
-	
+
 	public boolean fill_All(int i_account_id, String i_account_type, String i_first_name, String i_surname, String i_pw, boolean i_gender, String i_phone, String i_add, String i_mail){
 		accountID		= i_account_id;
 		accountType		= i_account_type;
@@ -39,29 +40,29 @@ public class AccountMessage{
 		System.out.println(address);
 		System.out.println(email);
 	}
-	
+
 	public ResponseMessage validateParams(){
 		ResponseMessage response = new ResponseMessage();
-		//response.responseCode = ResponseMessage.ResponseCode.SUCCESS;
+		response.responseString = "";
 		Matcher m;
 		// Verify accountType
 		// guest, maid, customer, staff, or admin
-		if(!(accountType.equalsIgnoreCase("guest") ||
+		if(accountType!=null && !(accountType.equalsIgnoreCase("guest") ||
 				accountType.equalsIgnoreCase("customer") ||
 				accountType.equalsIgnoreCase("maid") ||
 				accountType.equalsIgnoreCase("staff") ||
 				accountType.equalsIgnoreCase("admin")))
 		{
-			response.responseString = response.responseString + ("Invalid account type: " + accountType + ". Account id: " + accountID + "\n");
+			response.responseString = response.responseString + ("- Invalid account type: " + accountType + ". Account id: " + accountID + "<br />");
 			//CHANGE RESPONSE CODE
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 
-		
+
 		// Verify password
 		if(password.isEmpty())
 		{
-			response.responseString = response.responseString +  ("Password should not be empty. \n");
+			response.responseString = response.responseString +  ("- Password should not be blank. <br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 		else
@@ -70,23 +71,28 @@ public class AccountMessage{
 			m = validPwd.matcher(password);
 				if(!m.matches() || (m.groupCount() > 1))
 			{
-					response.responseString = response.responseString +  ("Password string is not vaild \n");
+					response.responseString = response.responseString +  ("- Password string is not vaild <br />");
 					response.responseCode = ResponseMessage.ResponseCode.FAIL;
-			}			
+			}
+			if(!password.equals(re_password))
+			{
+				response.responseString = response.responseString +  ("- Passwords do not match<br />");
+				response.responseCode = ResponseMessage.ResponseCode.FAIL;
+			}
 		}
-		
+
 		// Verify address
 		if(address.isEmpty())
 		{
-			response.responseString = response.responseString + ("Address field should not be blank.\n");			
+			response.responseString = response.responseString + ("- Address field should not be blank.<br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 
-		
+
 		// Verify email
 		if(email.isEmpty())
 		{
-			response.responseString = response.responseString + ("Email address should not be empty. \n" + "Account ID: " + accountID);
+			response.responseString = response.responseString + ("- Email address should not be blank. <br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 		else
@@ -95,7 +101,7 @@ public class AccountMessage{
 			m = validEmail.matcher(email);
 				if(!m.matches() || (m.groupCount() > 1))
 			{
-			response.responseString = response.responseString + ("Email address is not vaild.\n"); 
+			response.responseString = response.responseString + ("- Email address is not vaild.<br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 			}
 		}
@@ -103,49 +109,49 @@ public class AccountMessage{
 		// Verify First Name
 		if(firstName.isEmpty())
 		{
-			response.responseString = response.responseString + ("First Name should not be empty. \n");
+			response.responseString = response.responseString + ("- First Name should not be blank. <br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 		else {
 			Pattern validName = Pattern.compile("[a-zA-Z-]+");
 			m = validName.matcher(firstName);
 			if (!m.matches() || (m.groupCount() > 1)) {
-				response.responseString = response.responseString + ("First Name is not valid.\n");
+				response.responseString = response.responseString + ("- First Name is not valid.<br />");
 				response.responseCode = ResponseMessage.ResponseCode.FAIL;
 			}
 		}
-		
+
 		// Verify Last Name
 		if(lastName.isEmpty())
 		{
-			response.responseString = response.responseString + ("Last Name should not be empty. \n");
+			response.responseString = response.responseString + ("- Last Name should not be blank. <br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 		else {
 			Pattern validName = Pattern.compile("[a-zA-Z-]+");
 			m = validName.matcher(lastName);
 			if (!m.matches() || (m.groupCount() > 1)) {
-				response.responseString = response.responseString + ("Last Name is not valid. \n");
+				response.responseString = response.responseString + ("- Last Name is not valid. <br />");
 				response.responseCode = ResponseMessage.ResponseCode.FAIL;
 			}
-		}		
-		
+		}
+
 		// Verify Phone
 		if(phone.isEmpty())
 		{
-			response.responseString = response.responseString + ("Phone number should not be empty. \n");
+			response.responseString = response.responseString + ("- Phone number should not be blank. <br />");
 			response.responseCode = ResponseMessage.ResponseCode.FAIL;
 		}
 		else {
-			Pattern validPhone = Pattern.compile("[\\d-]+");
+			Pattern validPhone = Pattern.compile("\\+?[\\d-\\s]+");
 			m = validPhone.matcher(phone);
 			if (!m.matches() || (m.groupCount() > 1)) {
-				response.responseString = response.responseString + ("Phone number is not valid. \n");
+				response.responseString = response.responseString + ("- Phone number is not valid. <br />");
 				response.responseCode = ResponseMessage.ResponseCode.FAIL;
 			}
 		}
-	
+
 		return response;
 	}
-	
+
 }
