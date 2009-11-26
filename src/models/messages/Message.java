eae6551967a410c4d8dc580fcebc53ab52640a17
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-26 07:48:40 +0000

    some comments

--- src/models/messages/Message.java
+++ src/models/messages/Message.java
@@ -1,6 +1,18 @@
+/*Message.java
+ * The class that holds specific message types for transmission across the system classes
+ * <to be filled with standardized stuff once all code is complete>
+ * 
+ * 
+ * 
+ * 
+ */
 package models.messages;
 
 public class Message {
+	//Arrays of each types of specific messages. For messages going out of the interface (i.e. User input), 
+	//only the first element of the array of the relevant type is used. For outgoing messages, i.e. messages 
+	//coming back to the user, the relevant message array may contain more than one message item (view all bookings)
+	//is an example
 	public HeaderMessage header;
 	public AccountMessage[] accounts;
 	public RoomMessage[] rooms;
@@ -10,6 +22,8 @@ public class Message {
 	public ResponseMessage response;
 	public ReportMessage[] reports;
 
+	//The only constructor
+	//Authorization level of user and hotel name are stored in the message header
 	public Message(int i_authlevel, int i_mOID, String i_hotelname){
 		header= new HeaderMessage(i_authlevel, i_mOID, i_hotelname);
 		accounts=null;
@@ -19,6 +33,9 @@ public class Message {
 		extras=null;
 		response=new ResponseMessage();
 	}
+	
+	//Interface and any other class that wishes to use one of the specific message types 
+	//will have to initialize the array with the desired number of message elements to store
 	public void initializeAccounts(int arraysize){
 		accounts=new AccountMessage[arraysize];
 		for(int i=0; i<arraysize; i++){
@@ -56,6 +73,9 @@ public class Message {
 		}
 	}
 	
+	//This function validates the parameters in a specific message type.
+	//Which specific message type is used is determined by which of the message type arrays
+	//is not empty. If not empty (i.e. not null), it contains one of the specific message.
 	public boolean validate() {
 		if(accounts!=null) {
 			response = accounts[0].validateParams();
