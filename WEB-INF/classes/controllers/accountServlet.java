970f66d69cf086c27597c67ff4fb8a4acc548d62
michael.soderling <michael.soderling@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-26 07:02:51 +0000

    Added support for the edit functionality

--- WEB-INF/classes/controllers/accountServlet.java
+++ WEB-INF/classes/controllers/accountServlet.java
@@ -21,8 +21,17 @@ public class accountServlet extends HttpServlet {
 		String action = request.getParameter("action");	
 		if(action!=null && action.equals("view")) {
 			viewAccount(request, response);
-		} else {
+		} 
+		else if(action!=null && action.equals("viewCurrent")){
 			viewCurrentAccount(request, response);
+		}
+		else if(action!=null && action.equals("edit"))
+		{
+			editAccount(request, response);
+		}
+		else if(action!=null && action.equals("doEdit"))
+		{
+			doEditAccount(request, response);
 		}
 	}
 
@@ -80,6 +89,75 @@ public class accountServlet extends HttpServlet {
 			request.setAttribute("message", reply.response.responseString);
 			getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
 		}
+	}
+	
+	public void editAccount(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		int authlevel = ((Integer)request.getAttribute("authLevel")).intValue();
+		int userid = ((Integer)request.getAttribute("userID")).intValue();
+		String hotelname = (String)request.getAttribute("hotelName");
+		
+		int accountID = Integer.parseInt(request.getParameter("id"));
+		Message message = new Message(authlevel, userid, hotelname);
+		message.initializeAccounts(1);
+		message.accounts[0].accountID = accountID;
+		Account account = new Account();
+		if(authlevel >= 3 || message.accounts[0].accountID==userid) 
+		{	
+			Message reply = account.getAccount(message);
+			
+			if(reply.response.responseCode==ResponseMessage.ResponseCode.SUCCESS && reply.accounts.length > 0) 
+			{
+				request.setAttribute("account", reply.accounts[0]);
+				getServletContext().getRequestDispatcher("/views/edit_account_form.jsp").include(request, response);
+			}
+			else 
+			{
+				request.setAttribute("message", reply.response.responseString);
+				getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
+			}
+		}
+		else 
+		{
+			request.setAttribute("message", "You are not authorized to edit this account.");
+			getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
+		} 
+	}
+	
+	public void doEditAccount(HttpServletRequest request, HttpServletResponse response)
+	throws IOException, ServletException
+	{
+		int authlevel = ((Integer)request.getAttribute("authLevel")).intValue();
+		int userid = ((Integer)request.getAttribute("userID")).intValue();
+		String hotelname = (String)request.getAttribute("hotelName");
+		
+		int accountID = Integer.parseInt(request.getParameter("id"));
+		Message message = new Message(authlevel, userid, hotelname);
+		message.initializeAccounts(1);
+		message.accounts[0].accountID = accountID;
+		Account account = new Account();
+		if(authlevel >= 3 || message.accounts[0].accountID==userid) 
+		{	
+			Message reply = account.editAccount(message);
+			
+			if(reply.response.responseCode==ResponseMessage.ResponseCode.SUCCESS && reply.accounts.length > 0) 
+			{
+				request.setAttribute("account", reply.accounts[0]);
+				request.setAttribute("status", "edit_account_success");
+				getServletContext().getRequestDispatcher("/views/edit_account_success.jsp").include(request, response);
+			}
+			else 
+			{
+				request.setAttribute("message", reply.response.responseString);
+				getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
+			}
+		}
+		else 
+		{
+			request.setAttribute("message", "You are not authorized to edit this account.");
+			getServletContext().getRequestDispatcher("/views/error.jsp").include(request, response);
+		} 
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
