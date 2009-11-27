f8a31598e882b15ec4a9bd0bbca2e951f40a19e7
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-27 21:26:53 +0000

    added comments

--- branches/Database_Final_v1/src/models/database/databaseHelper.java
+++ branches/Database_Final_v1/src/models/database/databaseHelper.java
@@ -1,3 +1,9 @@
+/*
+ * DatabaseHelper.java
+ * 
+ * This class handles database transactions for all the keeper classes (Account.java, Bill.java, ..)
+ * Provides functions for adding, editing, retrieving, an deleting database entries
+ */
 package models.database;
 
 import java.sql.*;
@@ -11,11 +17,27 @@ public class databaseHelper {
 
 	private Connection connection;
 
+	
+	/*
+	 * PRE: None
+	 * POST: Returns a databaseHelper object
+	 * 
+	 * The default constructor
+	 */
 	public databaseHelper() throws SQLException, ClassNotFoundException
   	{
     	Class.forName(jdbcDriver); //set Java database connectivity driver
     	connection = DriverManager.getConnection(dbURL, username, password);
 	}
+	
+	
+	/*
+	 * PRE: String is not empty and is formatted to mySQL specs
+	 * POST: Returns a ResultSet that contains requested database entries else throws exception
+	 * 
+	 * 	
+	 * The function used to retrieve (get/getAll) entries from the database
+	 */
 
 	public ResultSet select(String query)throws SQLException
 	{
@@ -30,11 +52,29 @@ public class databaseHelper {
 	    return st.executeUpdate();
 	}
 
+	
+	/*
+	 * PRE: String is not empty and is formatted to mySQL specs. Database connection has been opened
+	 * POST: The database update entry request is performed else throws exception
+	 * 
+	 * 	
+	 * This function is used to edit and delete database entries
+	 */
 	public int update(String statement)throws SQLException
 	{
 		PreparedStatement st  = connection.prepareStatement(statement);
 	    return st.executeUpdate();
 	}
+	
+	
+	
+	/*
+	 * PRE: String is not empty and is formatted to mySQL specs. Database connection has been opened
+	 * POST: The database insert entry request is performed else throws exception
+	 * 
+	 * 	
+	 * This function is used to add entries to the database
+	 */
 	public int insert(String query)throws SQLException
 	{
 		PreparedStatement st  = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
@@ -46,6 +86,16 @@ public class databaseHelper {
 			return 0;
 		}
 	}
+	
+	
+	
+	/*
+	 * PRE: Database connection is opened
+	 * POST: The database connection is closed
+	 * 
+	 * 	
+	 * This function is used to close the database connection once opened
+	 */
 	public void close()
 	{
 		try
