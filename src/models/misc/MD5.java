32f94e84b94538566d860e01725371d2e1c836f7
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-27 08:38:06 +0000

    added comments

--- src/models/misc/MD5.java
+++ src/models/misc/MD5.java
@@ -1,7 +1,23 @@
+/*
+ * MD5.java
+ * 
+ * This is the class used to encrypt sensitive information (passwords) in a format
+ * in which any user cannot view the password in plain text
+ */
+
 package models.misc;
 import java.security.*;
 
 public class MD5 {
+	
+	
+	/*
+	 * PRE: String is not empty. 
+	 * POST: Returns a hash string of the password
+	 * 
+	 * 	
+	 * Gets the non plain text string of the password which is what will be stored in the database 
+	 */
 	public static String hashString(String pwd) {
 		StringBuffer hex = new StringBuffer();
 		try {
