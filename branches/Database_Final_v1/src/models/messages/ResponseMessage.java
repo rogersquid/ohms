8ec5ce385e221daa48cf490cfe5e05c14a3f0289
thinkmaster <thinkmaster@46487674-b8f2-11de-b017-a9a2c7051f87>
2009-11-27 07:57:51 +0000

    added comments

--- branches/Database_Final_v1/src/models/messages/ResponseMessage.java
+++ branches/Database_Final_v1/src/models/messages/ResponseMessage.java
@@ -1,9 +1,27 @@
+/* ResponseMessage.java
+ * 
+ * This object is used to accrue response messages when actions are performed on the
+ * specific message types(ex. validate parameters: this would be a return type with the
+ * string field filled with descriptions of the errors in the parameters).
+ *  
+ * 
+ */
 package models.messages;
+
+
 
 public class ResponseMessage {
 	public enum ResponseCode { SUCCESS, FAIL }
 	public ResponseCode responseCode;
 	public String responseString;
+
+	
+
+	/*
+	 * PRE: NONE
+	 * POST: The responseCode and responseString fields are filled out 
+	 * 
+	 */
 	public void fillResponse(ResponseCode resCode, String resStr){
 		responseCode=resCode;
 		responseString=resStr;
